2ab6abc0b008091126b7bfb657057ab43a63c890
chuy-zip <andreschuy03@gmail.com>
2025-01-25 23:01:30 -0600

    another idea

--- src/buffer.py
+++ src/buffer.py
@@ -4,7 +4,7 @@
 Integrantes del grupo:
 Nelson García 22434
 Joaquín Puente 22296
-Ricardo Chuy 22
+Ricardo Chuy 221007
 Diego Linares 22
 """
 
@@ -15,24 +15,37 @@ def cargar_buffer(entrada, inicio, tamano_buffer):
     buffer.append("eof")
   return buffer
 
-def procesar_buffer(buffer):
+def procesar_buffer(buffer, avance, lexema):
+    
     # Procesar y extraer lexemas del buffer
-    lexema = ""
     for i in range(len(buffer)):
-        if buffer[i] == "eof":
-            break
-        elif buffer[i] == " ":
+        
+        if buffer[i] == " ":
            print("Lexema procesado: " + lexema)
            lexema = ""
+           print("Valor de avance: ", avance)
+
+        elif lexema == "eof":
+           print("Lexema procesado: " + lexema)
+           break
+           
         else:
             lexema += buffer[i]
         
-    
+        avance += 1
+        
+    return lexema, avance 
 
 entrada = list("Esto es un ejemplo de entrada con eof")
 inicio = 0
 avance = 0
+
+ultimo_lex = ""
 tamano_buffer = 10
-buffer = cargar_buffer(entrada, inicio, tamano_buffer)
-print(buffer)
-procesar_buffer(buffer)
+
+while ultimo_lex != "eof":
+
+  buffer = cargar_buffer(entrada, inicio, tamano_buffer)
+  print(buffer)
+  ultimo_lex, inicio = procesar_buffer(buffer, avance, ultimo_lex)
+  avance = inicio
