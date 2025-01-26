d723b1a6c1a256003ff59fb3b33d9dea3b04a2a6
Its-Japo <pue22296@uvg.edu.gt>
2025-01-25 22:27:39 -0600

    Practica terminada

--- src/buffer.py
+++ src/buffer.py
@@ -4,40 +4,53 @@
 Integrantes del grupo:
 Nelson García 22434
 Joaquín Puente 22296
-Ricardo Chuy 22
+Ricardo Chuy 221007
 Diego Linares 22
 """
 
-# Código base para iniciar
-def cargar_buffer(entrada, inicio, tamano_buffer):
-	buffer = entrada[inicio:inicio + tamano_buffer]
-	if len(buffer) < tamano_buffer:
-		buffer.append("eof")
-		avance += len(buffer)    
-	return buffer
 
-def procesar_buffer(buffer):
+# Código base para iniciar
+def cargar_buffer(entrada: list[str], inicio: int, tamano_buffer: int) -> (list[str], int):
+    buffer: list[str] = entrada[inicio:inicio + tamano_buffer]
+    if len(buffer) < tamano_buffer:
+        buffer.append("eof")
+    return buffer
+
+
+def procesar_buffer(lexema, buffer):
     # Procesar y extraer lexemas del buffer
-    lexema = ""
+    avance: int = 0
     for i in range(len(buffer)):
+        avance += 1
         if buffer[i] == "eof":
-            break
+            print("Lexema procesado: " + lexema)
+            return None, avance
         elif buffer[i] == " ":
-           print("Lexema procesado: " + lexema)
-           lexema = ""
+            print("Lexema procesado: " + lexema)
+            lexema = ""
         else:
             lexema += buffer[i]
 
-def main ():
+    return lexema, avance
+
+
+def main():
+    inicio = 0
+    avance = 0
+    tamano_buffer = 10
+    parsing = True
+    lexema = ""
+
     entrada = list("Esto es un ejemplo de entrada con eof")
     print(len(entrada))
-    buffer = cargar_buffer(entrada, inicio, tamano_buffer)
-    print("Buffer cargado: " + str(buffer))
-    procesar_buffer(buffer)
-    inicio = avance
-        
-inicio = 0
-avance = 0
-tamano_buffer = 10
+    while parsing:
+        buffer = cargar_buffer(entrada, inicio, tamano_buffer)
+        lexema, avance = procesar_buffer(lexema, buffer)
+        if lexema is None:
+            break
 
-main()
+        inicio += avance
+
+
+if __name__ == '__main__':
+    main()
